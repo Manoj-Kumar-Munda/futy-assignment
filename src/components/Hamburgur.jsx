600d5f80da21c83de4a2c1e7751b34476e45786a
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-09-29 11:19:27 +0530

    searchbar animation done

--- src/components/Hamburgur.jsx
+++ src/components/Hamburgur.jsx
@@ -1,37 +1,51 @@
 import React, { useState } from "react";
 import { cn } from "../utils/cn";
+import { AnimatePresence, motion } from "framer-motion";
 
-const Hamburgur = () => {
+const Hamburgur = ({ isSearchBarOpen }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <button className="w-14 h-14 relative focus:outline-none brounded" onClick={() => setIsOpen(prev => !prev)}>
-      <div
-        className={
-          "block w-5 absolute left-6 top-1/2   transform  -translate-x-1/2 -translate-y-1/2"
-        }
+    // <AnimatePresence>
+    <>
+      <button
+        // initial={{ opacity: 0 }}
+        // animate={{ opacity: 1 }}
+        // transition={{ delay: 0.7 }}
+        // exit={{ opacity: 0 }}
+        // key={Math.random()}
+        className={cn("transition-all duration-500 w-12 h-12 relative focus:outline-none rounded-full", isSearchBarOpen && "opacity-0")}
+        
+        onClick={() => setIsOpen((prev) => !prev)}
       >
-        <span
-          className={cn(
-            "block absolute h-0.5 w-7 text-white bg-current transform transition duration-500 ease-in-out",
-            isOpen && "rotate-45",
-            !isOpen && "-translate-y-1.5"
-          )}
-        ></span>
-        <span
-          className={cn(
-            "block absolute  h-0.5 w-5 text-white bg-current   transform transition duration-500 ease-in-out",
-            isOpen && "opacity-0"
-          )}
-        ></span>
-        <span
-          className={cn(
-            "block absolute  h-0.5 w-7 text-white bg-current transform  transition duration-500 ease-in-out",
-            isOpen && "-rotate-45",
-            !isOpen && "translate-y-1.5"
-          )}
-        ></span>
-      </div>
-    </button>
+        <div
+          className={
+            "block w-5 absolute left-6 top-1/2   transform  -translate-x-1/2 -translate-y-1/2"
+          }
+        >
+          <span
+            className={cn(
+              "block absolute h-0.5 w-7 text-white bg-current transform transition duration-500 ease-in-out",
+              isOpen && "rotate-45",
+              !isOpen && "-translate-y-1.5"
+            )}
+          ></span>
+          <span
+            className={cn(
+              "block absolute  h-0.5 w-5 text-white bg-current   transform transition duration-500 ease-in-out",
+              isOpen && "opacity-0"
+            )}
+          ></span>
+          <span
+            className={cn(
+              "block absolute  h-0.5 w-7 text-white bg-current transform  transition duration-500 ease-in-out",
+              isOpen && "-rotate-45",
+              !isOpen && "translate-y-1.5"
+            )}
+          ></span>
+        </div>
+      </button>
+    </>
+    // </AnimatePresence>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import Profile from "../assets/profile.png";
 import Hamburgur from "./Hamburgur";
-const Header = () => {
-  return (
-    <header className="py-2 px-2 border flex justify-between items-center">
-      <div className="bg-white rounded-full  w-12 h-12 p-1.5">
-        <img
-          src={Profile}
-          alt="profile"
-          className="w-full h-full object-cover"
-        />
-      </div>
+import { IoSearchOutline } from "react-icons/io5";
+import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "../utils/cn";
 
-      <Hamburgur />
+const variants = {
+  open: { opacity: 1 },
+  closed: { opacity: 0 },
+};
+
+const Header = () => {
+  const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
+  return (
+    <header className="relative py-2 px-2 h-12 border-b border-white/20 flex justify-between items-center">
+      <AnimatePresence>
+        {!isSearchBarOpen && (
+          <motion.div
+            // initial={{ opacity: 0 }}
+            // animate={{ opacity: 1 }}
+            // exit={{ opacity: 0 }}
+            // transition={{ delay: 0.5 }}
+            // key={Math.random()}
+            className="flex gap-2"
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ delay: .75 }}
+              key={Math.random()}
+              className="bg-white rounded-full  w-10 h-10 p-1.5"
+            >
+              <img
+                src={Profile}
+                alt="profile"
+                className="w-full h-full object-cover"
+              />
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ delay: .75 }}
+              key={Math.random()}
+              className="border-2 inline-flex items-center gap-4  text-red-600 my-1 rounded-2xl px-1 border-red-600"
+            >
+              <div className="font-extrabold rounded-full inline-flex justify-center items-center w-5 h-5 border-red-600 border-2 text-sm italic font-sans">
+                F
+              </div>
+              <span className="font-extrabold text-sm">50</span>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <div className="flex items-center gap-2">
+        <div
+          className={cn(
+            "transition-all delay-200 duration-300 rounded-full  flex justify-center items-center absolute right-14 left-2",
+            isSearchBarOpen && "right-2 left-2"
+          )}
+        >
+          <AnimatePresence>
+            <motion.div
+              style={{ transformOrigin: "right" }}
+              initial="close"
+              animate={
+                isSearchBarOpen
+                  ? { opacity: 1, scaleX: 1 }
+                  : { opacity: 0, scaleX: 0 }
+              }
+              transition={{ delay: 0.6, type: "tween" }}
+              variants={variants}
+              className="bg-white rounded-xl overflow-hidden w-full px-4 flex"
+            >
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-full border rounded-xl  bg-white py-0.5 text-black outline-none"
+              />
+
+              <select className="border border-black my-1 text-black rounded-full px-1 outline-none">
+                {/* <option disabled selected>Search by</option> */}
+                <option value={"Team"}>Team</option>
+                <option value={"Date"}>Date</option>
+              </select>
+            </motion.div>
+          </AnimatePresence>
+
+          <button
+            onClick={() => setIsSearchBarOpen((prev) => !prev)}
+            className={cn(
+              "w-10 h-10 rounded-full hover:bg-white/10 flex justify-center items-center shrink-0"
+            )}
+          >
+            <IoSearchOutline size={20} />
+          </button>
+        </div>
+
+        <Hamburgur isSearchBarOpen={isSearchBarOpen} />
+      </div>
     </header>
   );
 };
