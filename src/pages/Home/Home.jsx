812175c05632e0ac2f49a1f66407ac76a645b8eb
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-10-01 23:12:03 +0530

    added footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Header from "../../components/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
 
-      <div>
-
-      </div>
+      <div></div>
 
       <Outlet />
+
+      <Footer />
     </div>
   );
 };
